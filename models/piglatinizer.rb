500dd840f0798c66e773fadc39f665953530ffa5
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 15:56:31 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,13 @@
+class PigLatinizer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def latinize_words
+    words_array = text.split(" ")
+    binding.pry
+  end
+  
+end
