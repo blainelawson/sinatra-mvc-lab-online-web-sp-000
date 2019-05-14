a317d1ba06a04cb00e63fc54335a01edfb3ad5b8
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 18:50:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -37,10 +37,3 @@ class PigLatinizer
   end
 end
 
-# I have to:
-# split phrase into individual words
-# find first consonant
-# cut first part of word through the first consonant from string
-# removed first part and add to the end with 'ay'
-# add cut part to end of word with 'ay'
-# combine words together again
