9fedae790b341a376a7740a261931b2f37c888c2
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 16:28:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,9 +6,21 @@ class PigLatinizer
   end
 
   def latinize_words
-    binding.pry
     words_array = text.split(" ")
-    binding.pry
+    words_array.each do |word|
+      
+    
   end
 
+  def first_consonant(word)
+    consonant = word.scan(/[^aeouiAEOUI]/).first
+    
+
 end
+
+# I have to:
+# split phrase into individual words
+# find first consonant
+# cut first part of word through the first consonant from string
+# add cut part to end of word with 'ay'
+# combine words together again
